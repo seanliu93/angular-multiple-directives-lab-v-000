369ea77cf8c898e4feae2ad414a23c2c38386336
seanliu93 <sean93liu@gmail.com>
2016-06-16 20:13:34 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -13,6 +13,12 @@ function ContactController() {
     this.removeContact = function (index) {
         this.contacts.splice(index, 1);
     }
+    this.addContact = function () {
+         this.contacts.push({
+             name: this.name,
+             phone: this.phoneNumber
+         });
+     }
 }
 
 angular
